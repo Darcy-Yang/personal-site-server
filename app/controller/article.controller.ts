0a8f8e8471098d82c328a1e843ad0eec916543c4
yangliangfei <yangliangfei@oneband.life>
2020-01-30 21:55:46 +0800

    update: add `getArticle` route

--- app/controller/article.controller.ts
+++ app/controller/article.controller.ts
@@ -7,9 +7,29 @@ async function createArticle(ctx: any) {
   await Article.create({
     content
   })
-  ctx.body = { success: 1 }
+  ctx.body = { success: 1, message: '创建成功' }
+}
+
+// 考虑分页
+async function getArticles(ctx: any) {
+  const { search } = ctx.request.query
+  let result
+
+  if (search) {
+    const reg = new RegExp(search, 'i')
+    result = await Article.find({ content: reg })
+  } else {
+    result = await Article.find()
+  }
+
+  ctx.body = {
+    success: 1,
+    result,
+    message: '获取文章成功'
+  }
 }
 
 export default {
-  createArticle
+  createArticle,
+  getArticles
 }
--- app/router/article.router.ts
+++ app/router/article.router.ts
@@ -2,6 +2,8 @@ import Router from 'koa-router'
 import { article } from '../controller'
 
 const router = new Router({ prefix: '/article' })
-router.post('/', ctx => article.createArticle(ctx))
+router
+  .get('/', ctx => article.getArticles(ctx))
+  .post('/', ctx => article.createArticle(ctx))
 
 export default router
